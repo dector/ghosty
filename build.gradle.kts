c64a87fa245cd4cc7f3817190b2fc7f37a68aeff
Denys M <dector@dector.space>
2021-02-16 02:58:12 +0400

    Use Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+extra["compose_version"] = "1.0.0-alpha12"
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-alpha06")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+tasks.create("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
