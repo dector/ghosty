616d1c2bae0713cb45aa7b37623046defd9b9435
Denys M <dector@dector.space>
2021-02-16 23:21:36 +0400

    Send commands to device

--- app/src/main/java/space/dector/ghosty/MainActivity.kt
+++ app/src/main/java/space/dector/ghosty/MainActivity.kt
@@ -17,32 +17,65 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import space.dector.ghosty.ui.theme.AppTheme
 import space.dector.tuyalib.Bulb
+import space.dector.tuyalib.IpAddress
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val device = Bulb(
-        ip = BuildConfig.DEVICE_IP,
-        deviceId = BuildConfig.DEVICE_ID,
-        localKey = BuildConfig.DEVICE_LOCAL_KEY,
-    )
+    private val controller = object : DeviceController {
+
+        private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+        private val device = Bulb(
+            ip = BuildConfig.DEVICE_IP,
+            deviceId = BuildConfig.DEVICE_ID,
+            localKey = BuildConfig.DEVICE_LOCAL_KEY,
+        )
+
+        override val ip = device.ip
+
+        override fun turnOn() = execute {
+            device.turnOn()
+        }
+
+        override fun turnOff() = execute {
+            device.turnOff()
+        }
+
+        private fun execute(action: () -> Unit) {
+            scope.launch {
+                action()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             AppTheme {
-                MainScreen(device = device)
+                MainScreen(controller = controller)
             }
         }
     }
 }
 
+interface DeviceController {
+    val ip: IpAddress
+
+    fun turnOn()
+    fun turnOff()
+}
+
 @Composable
 private fun MainScreen(
-    device: Bulb,
+    controller: DeviceController,
 ) {
     Surface(
         color = MaterialTheme.colors.background,
@@ -50,18 +83,18 @@ private fun MainScreen(
     ) {
         Column {
             Row {
-                Text("IP: ${device.ip}")
+                Text("IP: ${controller.ip}")
             }
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center,
             ) {
-                Button(onClick = { /*device.turnOn()*/ }) {
+                Button(onClick = { controller.turnOn() }) {
                     Text("ON")
                 }
                 Spacer(modifier = Modifier.width(4.dp))
-                Button(onClick = { /*device.turnOff()*/ }) {
+                Button(onClick = { controller.turnOff() }) {
                     Text("OFF")
                 }
             }
