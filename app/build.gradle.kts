3e94b414d8c93edb5c41eafa9c4e4236d529a4f6
Denys M <dector@dector.space>
2021-02-25 02:17:37 +0400

    Update Material design library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation("androidx.compose.material:material:${Version.compose}")
     implementation("androidx.activity:activity-compose:1.3.0-alpha03")
 
+    implementation("com.google.android.material:material:1.3.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
 
     testImplementation("junit:junit:4.+")
